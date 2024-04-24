382d9902979851c4ba0641a1932c3d8d35bac635
Tom Lausmann <tom.lausmann@unitb.de>
2024-04-25 00:10:12 +0200

    feat: update colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,9 +6,20 @@ return {
 		vim.cmd.colorscheme("gruvbuddy")
 
 		local colorbuddy = require("colorbuddy")
-		local c = colorbuddy.colors
-		local Group = colorbuddy.Group
 
-		Group.new("GitSignsChange", c.orange:light(), nil)
+		local Color = colorbuddy.Color
+		Color.new("ln", "#333333")
+		Color.new("cc", "#222222")
+		Color.new("sl", "#111111")
+		local c = colorbuddy.colors
+
+		local Group = colorbuddy.Group
+		Group.new("GitSignsChange", c.orange:light())
+		Group.new("ColorColumn", nil, c.cc)
+		Group.new("StatusLine", c.cc, c.sl)
+		Group.new("EndOfBuffer", c.cc, nil)
+		Group.new("LineNrAbove", c.ln, nil)
+		Group.new("LineNrBelow", c.ln, nil)
+		Group.new("LineNr", c.orange:light(), nil)
 	end,
 }
